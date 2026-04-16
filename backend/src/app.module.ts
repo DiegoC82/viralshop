ab07fdcd4751776add68b2bc5aa1338e8d5ba508
Diego Collado <diegocollado1982@gmail.com>
2026-04-15 21:00:53 -0300

    fix: Agregar PrismaService a los providers en AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { VideosModule } from './videos/videos.module';
 import { UsersModule } from './users/users.module';
-import { ScheduleModule } from '@nestjs/schedule'; // 👈 1. Importamos el módulo
+import { ScheduleModule } from '@nestjs/schedule';
+import { PrismaService } from './prisma.service'; // 👈 1. Importamos el módulo
 import { ChatsController } from './chats/chats.controller';
 
 @Module({
   imports: [AuthModule, VideosModule, UsersModule, ScheduleModule.forRoot()],
   controllers: [AppController, ChatsController],
-  providers: [AppService],
+  providers: [AppService, PrismaService], // 👈 2. Agregamos PrismaService a los providers
 })
 export class AppModule {}
